66101281ff5d4c10cfaa17b35efb3473da4d9d30
Stanislav <stas@pda.in.ua>
2016-02-09 10:40:24 +0200

    create migration

--- migrations/m151117_120316_create_tables.php
+++ migrations/m151117_120316_create_tables.php
@@ -7,10 +7,41 @@ class m151117_120316_create_tables extends Migration
 {
     public function up()
     {
-        $this->createTable('clients', [
+        $this->createTable('blog_categories', [
             'id' => Schema::TYPE_PK,
-            'email' => Schema::TYPE_STRING . ' NOT NULL',
-            'hash' => Schema::TYPE_TEXT,
+            'parent_id' => Schema::TYPE_INTEGER,
+            'title' => Schema::TYPE_STRING,
+            'user_id' => Schema::TYPE_INTEGER,
+        ]);
+
+        $this->createTable('blog_records', [
+            'id' => Schema::TYPE_PK,
+            'category_id' => Schema::TYPE_INTEGER,
+            'author_id' => Schema::TYPE_INTEGER,
+            'title' => Schema::TYPE_STRING,
+            'intro' => Schema::TYPE_TEXT,
+            'article' => Schema::TYPE_TEXT,
+            'date' => Schema::TYPE_DATETIME,
+        ]);
+
+        $this->createTable('blog_tags', [
+            'id' => Schema::TYPE_PK,
+            'title' => Schema::TYPE_STRING,
+        ]);
+
+        $this->createTable('blog_tags_relations', [
+            'id' => Schema::TYPE_PK,
+            'tag_id' => Schema::TYPE_INTEGER,
+            'record_id' => Schema::TYPE_INTEGER,
+        ]);
+
+        $this->createTable('images', [
+            'id' => Schema::TYPE_PK,
+            'type' => Schema::TYPE_SMALLINT,
+            'record_id' => Schema::TYPE_INTEGER,
+            'title' => Schema::TYPE_STRING,
+            'description' => Schema::TYPE_TEXT,
+            'file_name' => Schema::TYPE_STRING,
         ]);
 
         /*
@@ -23,7 +54,11 @@ class m151117_120316_create_tables extends Migration
 
     public function down()
     {
-        $this->dropTable('clients');
+        $this->dropTable('blog_categories');
+        $this->dropTable('blog_records');
+        $this->dropTable('blog_tags');
+        $this->dropTable('blog_tags_relations');
+        $this->dropTable('images');
     }
 
     /*
